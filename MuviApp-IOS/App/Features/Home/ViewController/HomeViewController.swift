d2f0fc7333d130a6bf74f5d98fbf3f74f4328214
uwaisalqadri <uwaisalqadri654321@gmail.com>
2021-08-25 12:44:22 +0700

    add section in home table

--- MuviApp-IOS/App/Features/Home/ViewController/HomeViewController.swift
+++ MuviApp-IOS/App/Features/Home/ViewController/HomeViewController.swift
@@ -9,13 +9,26 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+  private let homeView = HomeView()
+
+  private enum HomeTableSection {
+    case banner
+    case popular
+    case upcoming
+  }
+
+  private let allSection: [HomeTableSection] = [
+    .banner,
+    .popular,
+    .upcoming
+  ]
+
   override func viewWillAppear(_ animated: Bool) {
     setNavigationBar(type: .logoAndIcon)
   }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    let homeView = HomeView()
     view = homeView
     homeView.tableView.delegate = self
     homeView.tableView.dataSource = self
@@ -25,20 +38,30 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 3
+    return 1
+  }
+
+  func numberOfSections(in tableView: UITableView) -> Int {
+    allSection.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    switch indexPath.row {
-    case 0:
-      let cell: HomeBannerViewCell = tableView.dequeueReusableCell(for: indexPath)
-      cell.selectionStyle = .none
-      return cell
-    default:
+    let cell: HomeBannerViewCell = tableView.dequeueReusableCell(for: indexPath)
+    cell.selectionStyle = .none
+
+    switch allSection[indexPath.section] {
+    case .popular:
       let cell: HomeViewCell = tableView.dequeueReusableCell(for: indexPath)
       cell.lblCategory.text = "Popular"
-      cell.selectionStyle = .none
       return cell
+    case .upcoming:
+      let cell: HomeViewCell = tableView.dequeueReusableCell(for: indexPath)
+      cell.lblCategory.text = "Coming Soon"
+      return cell
+    default:
+      break
     }
+
+    return cell
   }
 }
